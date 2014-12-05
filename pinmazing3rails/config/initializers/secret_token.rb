4ccc2fd3cf7322a0491ff473648a7ccdb986acf3
coderletics-greg <greg.aubert@yahoo.co.uk>
2014-12-05 19:06:35 +0000

    installing rails

---
+++ pinmazing3rails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinmazing3rails::Application.config.secret_key_base = '5e3a41a488778ae1da5f7f645a16a5241a99869a7d3f459c9ad6ba38fbfc53bfccd8b61b32b3f4bbce413b739a5c07563c96d5453a7989ced8edbe04c98fd5f8'
---
+++ pinmazing3rails/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pinmazing3rails::Application.config.session_store :cookie_store, key: '_pinmazing3rails_session'
